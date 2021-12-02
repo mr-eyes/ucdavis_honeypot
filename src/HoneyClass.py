335b62107c7e7bd05ab1faf9e32cb1d7460174c7
Kaustav Goswami <kggoswami@ucdavis.edu>
2021-12-02 01:24:36 -0800

    base: added the main honey pot class

--- src/HoneyClass.py
+++ src/HoneyClass.py
@@ -5,26 +5,43 @@ from DetectClass import DetectClass
 # We create a class which accepts an email body 
 
 class HoneySpawnClass:
-    def __init_(self, email_id, instance_id):
-        self.email_id = email_id
+    def __init__(self, name, domain, instance_id):
+        self.name = name
+        self.domain = domain
         self.instance_id = instance_id
+    
+    def _generateEmailID(name, domain):
+        """
+            method generateEmailID(str, domain) accepts a name string separated
+            by a space and a domain name which should be in the format
+            domain.com.
+        """
+        # We use the name -> email format used at UC Davis.
+        #
+        # UC Davis uses the format stated below for emails.
+        # If the person's name is John Doe, his email address will be:
+        #           jdoe@ucdavis.edu
+        #
+        # If jdoe@ucdavis.edu is taken, then we arrive at the following
+        # scenario:
+        # The person's name is Jane Doe, we will resolve this conflict by:
+        #           jadoe@ucdavis.edu
+        # Conflicts are resolved by the HoneyUtil class.
 
-    def generateEmailIDs(names):
-        """
-            method generateEmailIDs(List) accepts a list of names separated by
-            a space.
-        """
-        emails = []
-        EMAIL_DOMAIN = "@ucdavis.edu"
-        for name in names:
-            # UC Davis uses the format stated below for emails.
-            # If the person's name is John Doe, his email address will be:
-            #           jdoe@ucdavis.edu
-            #
-            # If jdoe@ucdavis.edu is taken, then we arrive at the following
-            # scenario:
-            # The person's name is Jane Doe, we will resolve this conflict by:
-            #           jadoe@ucdavis.edu
-            email.append
-    def setPhantomEmailID():
-        return 
+        assert "@" not in domain
+
+        first_name = name.split(" ")[0]
+        last_name  = name.split(" ")[len(name.split(" ")) - 1]
+
+        # Check whether the first and the last names are valid.
+
+        if first_name == "" or last_name == "":
+            raise Exception("Incorrect names detected!")
+            exit(-1)
+
+        # Otherwise resume the program.
+
+        return first_name[0] + last_name + "@" + domain 
+
+    def setPhantomEmailID(self, name, domain):
+        return self._generateEmailID(name, domain)
