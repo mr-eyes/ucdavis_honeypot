44da3996e5ce47fbf61dc1e2f14973da8fbdbeb9
mr-eyes <mabuelanin@gmail.com>
2021-12-07 10:34:19 -0800

    send demo

--- src/send_emails.py
+++ src/send_emails.py
@@ -1,9 +1,11 @@
 from smtplib import SMTP as Client
+import time
 
 client = Client('127.0.0.1', 10025)
 
 
 for i in range(1, 11, 1):
-    r = client.sendmail('mahussien@ucdavis.edu', ['bishop@ucdavis.edu'], f"""\
-        Hi Matt, this is the email number {i}
+    r = client.sendmail(f"scammer_{i}@gmail.com", ['recipient@ucdavis.edu'], f"""\
+        Hi, this is a job offer for you! you will get paid $45/hr! Apply now!
     """)
+    time.sleep(1)
